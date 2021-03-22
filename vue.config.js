8f5b9b3a19777906627b379d3b21b4665e716ac5
jacckmark <jacckmark@gmail.com>
2021-03-22 21:28:57 +0100

    removed global styles (not used even once in proj for now)

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/assignment1/" : "/",
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `
-        @import "@/assets/styles/global.scss";
-        `,
-      },
-    },
-  },
 };
