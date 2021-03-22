92b32558208c0281b25237132213b69d3682165b
jacckmark <jacckmark@gmail.com>
2021-03-22 12:14:17 +0100

    changed route for this proj

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/olicom_assignment/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/assignment1/" : "/",
   css: {
     loaderOptions: {
       sass: {
